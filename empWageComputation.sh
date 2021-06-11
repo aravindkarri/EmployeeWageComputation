0a0bbcf1405b37a61318bdc993b089307db88a83
aravindkarri <aravindkarri98495@gmail.com>
2021-06-11 22:53:05 +0530

    Add: code to calculate EmployeeWage for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,20 +5,27 @@ echo "Welcome to Employee Wage Computation Program"
 #constants
 IS_FULL_TIME=1
 IS_PART_TIME=2
+TOTAL_WORKING_DAYS=20
 
 #variables
 empWagePerHr=20
-empCheck=$((RANDOM%3))
+salary=0
 
-case $empCheck in
-	$IS_FULL_TIME)
-		empHrs=8
-		;;
-	$IS_PART_TIME)
-		empHrs=4
-		;;
-	*)
-		empHrs=0
-		;;
-esac
-salary=$(($empHrs * $empWagePerHr))
+for ((day=1;day<=$TOTAL_WORKING_DAYS;day++))
+do
+	empCheck=$((RANDOM%3))
+	case $empCheck in
+		$IS_FULL_TIME)
+			empHrs=8
+			;;
+		$IS_PART_TIME)
+			empHrs=4
+			;;
+		*)
+			empHrs=0
+			;;
+	esac
+	dailyWage=$(($empHrs*$empWagePerHr))
+	salary=$(($salary+$dailyWage))
+done
+echo "Employee salary for a month: " $salary
